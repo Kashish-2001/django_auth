64499f5f82ffe1687ca95a6e9ec3527a87f73e6b
Kashish Agrawal <kashish.agrawal1907@gmail.com>
2021-10-14 18:09:16 +0530

    added password validation

--- auth/users/views.py
+++ auth/users/views.py
@@ -4,9 +4,12 @@ from rest_framework.response import Response
 
 
 class RegisterView(APIView):
-    def post(self, request):
+    @staticmethod
+    def post(request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
+            if len(request.data['password']) < 6:
+                return Response({"password": ["Password length must be atleast 6 characters"]})
             serializer.save()
         return Response(serializer.data)
 
