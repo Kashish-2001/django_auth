6bc3436d2181a5c707b31af2ed965f159410451e
Kashish Agrawal <kashish.agrawal1907@gmail.com>
2021-10-21 14:26:40 +0530

    update settings.py

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s-s3x0^^9bi^q277)ryc)6sassa#al%r+dhr&vcy0c&r)9carc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True  # Default
+DEBUG = False  # As we are putting this to server
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kashish-2001-django-auth.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -156,6 +158,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
